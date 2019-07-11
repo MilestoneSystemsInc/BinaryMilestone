658fdfeb9325ddfaf713bcba997b9888c28a5ac3
Sebastien Robitaille <srb@milestone.dk>
2019-07-11 11:30:02 -0400

    2019R1

---
+++ 2019R1/x64/CopyUIFiles.bat
@@ -0,0 +1,26 @@
+XCopy VideoOS.Platform.SDK.dll %1 /Y
+XCopy VideoOS.Platform.SDK.UI.dll %1 /Y
+XCopy VideoOS.Toolkit.dll %1 /Y
+XCopy VideoOS.UI.Common.WPF.dll %1 /Y
+XCopy VideoOS.UI.Common.WPF.VideoPlayer.dll %1 /Y
+XCopy mf*.dll %1 /Y
+XCopy ms*.dll %1 /Y
+XCopy xerces-c_3_1.dll %1 /Y
+XCopy xqilla2*.dll %1 /Y
+xCopy IMV*.dll %1 /Y
+XCopy boost_regex-vc120-mt-*.dll %1  /Y
+XCopy CoreToolkits.dll %1  /Y
+XCopy boost_thread-vc120-mt-*.dll %1  /Y
+XCopy boost_serialization-vc120-mt-*.dll %1  /Y
+XCopy boost_system-vc120-mt-*.dll %1  /Y
+XCopy icuuc48*.dll %1  /Y
+XCopy icudt48*.dll %1  /Y
+XCopy boost_chrono-vc120-mt-*.dll %1  /Y
+XCopy 15dd936825ad475ea34e35f3f54217a6\*.* %1\15dd936825ad475ea34e35f3f54217a6\ /y /s
+XCopy libmfx*.dll %1 /Y
+XCopy cudart64_80.dll %1 /Y
+XCopy nppc64_80.dll %1 /Y
+XCopy nppi64_80.dll %1 /Y
+XCopy Autofac.dll %1 /Y
+XCopy NAudio.dll %1 /Y
+
